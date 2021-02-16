9d2311353e012d568f80fb63965d550d44f7fe36
Diego Paulino <diegopaulino_3@hotmail.com>
2021-02-16 18:57:48 -0300

    agora sim resolve de verdade

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 var numbers = []
 
+function comparaValores(valorAtual, valorAnterior) {
+    return valorAtual - valorAnterior
+}
+
 function adicionar() {
     var inputNumber = document.getElementById('inputNumber')
     var select = document.getElementById('select')
     var option = document.createElement('option')
 
-    numbers.push(inputNumber.value)
-    numbers.sort()
+    numbers.push(Number(inputNumber.value))
     select.appendChild(option)
     option.text = `Valor ${inputNumber.value} adicionado`
+
 }
+
 
 function finalizar() {
     var result = document.getElementById('result')
     var smallestNumber = numbers[0]
     var biggestNumber = numbers[numbers.length - 1]
-
+    
+    numbers.sort(comparaValores)
     result.innerText = `
         Ao todo, temos ${numbers.length} números cadastrados. 
         O menor valor informado foi ${smallestNumber}. 
