f91d118fe4887d923089a3573ceb201a6691c416
Diego Paulino <diegopaulino_3@hotmail.com>
2021-02-16 18:11:12 -0300

    Corrige bug

--- script.js
+++ script.js
@@ -9,19 +9,17 @@ function adicionar() {
     numbers.sort()
     select.appendChild(option)
     option.text = `Valor ${inputNumber.value} adicionado`
-    
 }
 
 function finalizar() {
-    numbers.sort()
     var result = document.getElementById('result')
     var smallestNumber = numbers[0]
     var biggestNumber = numbers[numbers.length - 1]
-    result.innerText = `Ao todo, temos ${numbers.length} números cadastrados. O menor valor informado foi ${smallestNumber.value}. O maior valor informado foi ${biggestNumber.value}.`
 
-    console.log(numbers)
-    console.log(numbers[0])
-    //console.log(biggestNumber.value)
-
+    result.innerText = `
+        Ao todo, temos ${numbers.length} números cadastrados. 
+        O menor valor informado foi ${smallestNumber}. 
+        O maior valor informado foi ${biggestNumber}.
+    `
 }
 
